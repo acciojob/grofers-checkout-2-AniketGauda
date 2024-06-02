e0e233405bda137d5b5a95350166d6c8a9804687
Aniket Gauda <122367767+AniketGauda@users.noreply.github.com>
2024-06-03 03:18:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your code here
+let arr = document.getElementsByClassName("price");
+
+let sm=0;
+for(let i=0;i<arr.length;i++){
+    arr[i].setAttribute("data-ns-test","prices");
+    sm+=parseInt(arr[i].innerHTML);
+}
+
+let tp = document.createElement("tr");
+let ch = document.createElement("td");
+ch.setAttribute("data-ns-test","grandTotal");
+ch.setAttribute("id","gt");
+
+ch.textContent = `${sm}`;
+
+tp.appendChild(ch);
+
+document.getElementById("tbl").appendChild(tp);
